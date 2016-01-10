c159a61396cd0b2e9a26c5210212e2dd93849fd5
Jack <qq289192946@hotmail.com>
2016-01-10 02:03:20 -0500

    Add functions
    randomRBG(), randomTri(), inTriangle(),, findY()

--- ImageEvolution.py
+++ ImageEvolution.py
@@ -1,5 +1,7 @@
 import os, sys
-from PIL import Image
+from PIL import Image, ImageDraw
+from random import randint
+
 inputPath= 'picture.jpg'
 outputPath = 'altered.png'
 
@@ -47,3 +49,83 @@ def mutation(population):
         some of the individuals altered."""
 
 saveImage(evolveImage())
+
+"""
+JACK
+"""
+def randomRBG():
+    """returns a tuple with 3 random values"""
+    return (randint(0,255),randint(0,255),randint(0,255))
+
+def randomTri(height,width):
+    """
+    given height and width of image, generate a triangle with size relative to the dimensions
+    returns a list of tuples
+    """
+    x_offset = randint(-width/5,width)
+    y_offset = randint(-height/5,height)
+    #offset of the triangle
+    
+    factor = 2
+    #the maximum size of the triangle relative to dimensions. ie 1/factor
+    h = int(height / factor)
+    w = int(width / factor)
+
+    A = [randint(0, int(w/2)),randint(0,int(2*h/3))]
+    B = [randint(int(w/2), w),randint(0, int(2*h/3))]
+    C = [randint(0, w),randint(int(2*h/3), h)]
+
+    A[0] += x_offset
+    B[0] += x_offset
+    C[0] += x_offset
+    A[1] += y_offset
+    B[1] += y_offset
+    C[1] += y_offset
+
+    return [tuple(A),tuple(B),tuple(C)]
+        
+
+def inTriangle(pt_coord,tri_coord):
+    """
+    pt_coord is a list(point) containing 2 coordinates
+    tri_coord is a list containing 3 lists(points)
+
+    returns True if point is in triangle, False otherwise
+    """
+    x,y = pt_coord[0], pt_coord[1]
+    A = [tri_coord[0][0], tri_coord[0][1]]
+    B = [tri_coord[1][0], tri_coord[1][1]]
+    C = [tri_coord[2][0], tri_coord[2][1]]
+    
+    x_low = min(A[0], B[0],C[0])
+    x_hi = max(A[0], B[0],C[0])
+    if x not in range(x_low, x_hi):
+        return False
+    
+    y_low = min(A[1], B[1],C[1])
+    y_hi = max(A[1], B[1],C[1])
+    if y not in range(y_low, y_hi):
+        return False
+    
+    lines = []
+    if x in range(min(A[0],B[0]), max(A[0],B[0])):
+        lines.append([A,B])
+    if x in range(min(C[0],B[0]), max(C[0],B[0])):
+        lines.append([B,C])
+    if x in range(min(A[0],C[0]), max(A[0],C[0])):
+        lines.append([C,A])
+        
+    if y in range(findY(lines[0][0],lines[0][1],x), findY(lines[1][0],lines[1][1],x)):
+        return True
+    else:
+        return False
+
+
+def findY(A,B,x):
+    """
+    given a line formed by 2 points A and B
+    returns the value of y at x on that line
+    """
+    m = (B[1]-A[1]) / (B[0]-A[0])
+    b = A[1] - m*A[0]
+    return m*x + b
