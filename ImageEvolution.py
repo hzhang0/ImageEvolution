129f951f20bf01492d8d58db613261dd731afcb9
David Zhang <zhangh87@mcmaster.ca>
2016-02-20 00:13:10 -0500

    rewrote randomTri, implements initializePopulation.

--- ImageEvolution.py
+++ ImageEvolution.py
@@ -1,12 +1,14 @@
 import os, sys
 from PIL import Image, ImageDraw
-from random import randint
+from random import randint, uniform
 
 inputPath= 'picture.jpg'
 outputPath = 'altered.png'
 
 def evolveImage():
     """Given an image, returns an altered version of the image"""
+    img = readImage(inputPath)
+    width, height = img.size    
         
 def readImage(path):
     """Returns a PIL image object given a path."""
@@ -22,6 +24,32 @@ def initializePopulation(x,y):
         Each individual is a list of four lists.
         The first three lists are [x,y] coordinates.
         The fourth list corresponds to a [R,G,B] color."""
+    pop = []
+    numPop = (int)((x*y)**(0.5))
+    for i in range(numPop):
+        pop.append(randomTri(x,y))
+    curImg = popToImage(x,y,pop)
+    size = 0.3
+    offset= 0.3
+    tri = Image.new('RGBA', (x, y)) 
+    draw = ImageDraw.Draw(curImg)
+    draw_tri = ImageDraw.Draw(tri)
+    for i in range(x):
+        for j in range(y):
+            pixel = curImg.getpixel((i,j))
+            if pixel == (255,255,255,255):
+                c1 = [max((int)(-offset*x), (int)(i-size*x)), j]
+                c2 = [i,max((int)(-offset*y), (int)(j-size*y))]
+                c3 = [min((int)(x*(1+offset)), (int)(i+size*x)), j]
+                A = [randint(c1[0],c2[0]), randint(c2[1],c1[1])]
+                B = [randint(c2[0],c3[0]), randint(c2[1],c3[1])]
+                C = [i, randint(j+1, min((int)(j+size*y), (int)(y*(1+offset))))]
+                RGBA = [randint(0,255),randint(0,255),randint(0,255),randint(130,255)]
+                pop.append([A,B,C,RGBA])
+                draw_tri.polygon([(0, 0), (0, y), (x, y), (x, 0)], fill = (255,255,255,0))
+                draw_tri.polygon([tuple(A),tuple(B),tuple(C)], fill=tuple(RGBA))
+                curImg = Image.alpha_composite(curImg, tri)                
+    return pop
 
 def popToImage(x,y,population):
     """Input: x,y dimensions, the whole population
@@ -40,7 +68,7 @@ def popToImage(x,y,population):
     
     for i in range(num_pop):
         draw_tri.polygon([(0, 0), (0, y), (x, y), (x, 0)], fill = (255,255,255,0)) #update the triangle to a blank canvas
-        draw_tri.polygon([population[i][0],population[i][1],population[i][2]], fill = population[i][3])
+        draw_tri.polygon([tuple(population[i][0]),tuple(population[i][1]),tuple(population[i][2])], fill=tuple(population[i][3]))
         img = Image.alpha_composite(img, tri)
         
     return img
@@ -87,53 +115,32 @@ def evaluation(population, img, imgAltered):
     total /= (width/steps)*(height/steps)
     return scores, total
 
-def selection(population, fitness):
-    """Input: the entire population, fitness output from the evaluation function
-        Output: a population list with length less than the input"""
-
-def crossover(population):
+def crossover(population, scores):
     """Input: the entire population
         Output: a population list with length longer than the input"""
     
-def mutation(population):
+def mutation(population, scores):
     """Input: the entire population
         Output: a population list with some random properties of
         some of the individuals altered."""
-
-saveImage(evolveImage())
-
+    
 
 #utility functions
-def randomRBG(trans):
-    """returns a tuple with 3 random values"""
-    return (randint(0,255),randint(0,255),randint(0,255), trans)
-
-def randomTri(height,width):
+def randomTri(width,height):
     """
     given height and width of image, generate a triangle with size relative to the dimensions
-    returns a list of tuples
     """
-    x_offset = randint(-width/5,width)
-    y_offset = randint(-height/5,height)
-    #offset of the triangle
+    offset = 0.3
+    size = 0.6
+    A = [randint((int)(-width*offset), (int)(width*(1+offset))),randint((int)(-height*offset), (int)(height*(1+offset)))]
+    c1 = [max((int)(A[0]-size*width),(int)(-width*offset)), max((int)(A[1]-size*height),(int)(-height*offset))]
+    c2 = [min((int)(A[0]+size*width), (int)(width*(1+offset))), min((int)(A[1]+size*height), (int)(height*(1+offset)))]
+    B =  [randint(c1[0],c2[0]), randint(c1[1],c2[1])]
+    c3 = [min((int)(B[0]+size*width), (int)(width*(1+offset))), min((int)(B[1]+size*height), (int)(height*(1+offset)))]
+    C = [randint(c1[0],c3[0]), randint(c1[1],c3[1])]
     
-    factor = 2
-    #the maximum size of the triangle relative to dimensions. ie 1/factor
-    h = int(height / factor)
-    w = int(width / factor)
-
-    A = [randint(0, int(w/2)),randint(0,int(2*h/3))]
-    B = [randint(int(w/2), w),randint(0, int(2*h/3))]
-    C = [randint(0, w),randint(int(2*h/3), h)]
-
-    A[0] += x_offset
-    B[0] += x_offset
-    C[0] += x_offset
-    A[1] += y_offset
-    B[1] += y_offset
-    C[1] += y_offset
-
-    return [tuple(A),tuple(B),tuple(C)]
+    RGBA = [randint(0,255),randint(0,255),randint(0,255),randint(130,255)]    
+    return [A,B,C,RGBA]
     
 def inTriangle(pt_coord,tri_coord):
     """
@@ -181,3 +188,5 @@ def findY(A,B,x):
     m = (B[1]-A[1]) / (B[0]-A[0])
     b = A[1] - m*A[0]
     return m*x + b
+
+#saveImage(evolveImage())
